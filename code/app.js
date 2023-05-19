ed8d3401899b0b4db4a28e284d1b46f0f192ae5c
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2023-05-19 00:42:03 -0700

    reveal animation added

--- code/app.js
+++ code/app.js
@@ -1,1 +1,19 @@
+// on scroll side reveal animation for section two elements
 
+let reveal = () => {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i=0; i<reveals.length; i++){
+        let viewPort = window.innerHeight;
+        let boxTop = reveals[i].getBoundingClientRect().top;
+        let point = 150;
+
+        if(boxTop < viewPort - point){
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll",reveal);
