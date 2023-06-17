d43bedda1800540d2f186c3a3da30fc7a700dbe5
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2023-06-17 09:43:49 -0700

    functionality of adding product imge to cart

--- code/products/app.js
+++ code/products/app.js
@@ -8,6 +8,25 @@ let products = [
     "verkade","quality street","toblerone","twirl"
 ]; // a list of products name 
 
+let imgsrc = [
+    "./../../resource/pic/products/1.jpg",
+    "./../../resource/pic/products/caramels.jpg",
+    "./../../resource/pic/products/create-your-own-6-bar-letterbox-selection-450x450.jpg",
+    "./../../resource/pic/products/darkmaltesers.jpg",
+    "./../../resource/pic/products/ferrerorochertjpg.jpg ",
+    "./../../resource/pic/products/kitkat.jpg",
+    "./../../resource/pic/products/maltesers.jpg",
+    "./../../resource/pic/products/mandmprotein.jpg",
+    "./../../resource/pic/products/Milk-Chocolate-Bar.jpg",
+    "./../../resource/pic/products/kitkat.jpg",
+    "./../../resource/pic/products/shopping.png",
+    "./../../resource/pic/products/shopping2.png",
+    "./../../resource/pic/products/shopping3.png",
+    "./../../resource/pic/products/shopping4.png",
+    "./../../resource/pic/products/super-toblerone-giant-chocolate.jpg",
+    "./../../resource/pic/products/twirlorangeresized.png"
+];
+
 let addToCartButt = document.querySelectorAll("#addToCart"); // getting all the buttons on the prod page
 let prodOnCart = sessionStorage.getItem("prodnum"); // number of product selected
 
@@ -22,9 +41,13 @@ let addToCart = ( ) => {
 
 // function to create the replica of an added produnct
 
-let addpro = ( productName) => {
+let addpro = ( productName,img) => {
     let box = document.createElement("div"); // 
+    box.classList.add("box");
     let pname = document.createTextNode(productName);
+    let imgNode = document.createElement("img");
+    imgNode.src = img;
+    box.appendChild(imgNode);
     box.appendChild(pname);
     document.getElementById("cartsec").appendChild(box);
 }
@@ -45,7 +68,7 @@ window.addEventListener("load",() => {
 // loop to identify which product is selected and to listen for an event on all add to cart buttons 
 for (let i=0 ; i<addToCartButt.length; i++){
     addToCartButt[i].addEventListener("click",() => {
-        addpro(products[i]);
+        addpro(products[i],imgsrc[i]);
         addToCart();
     });
 }
